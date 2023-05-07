94a41a7012f102a788407144a79396bf6551b453
halo <halo@halo.com>
2023-05-07 21:33:29 +0800

    ✨ feat(UserFilter): Add text fields for list filtering

--- user-list/src/App.js
+++ user-list/src/App.js
@@ -1,26 +1,56 @@
 import { Fragment, useState, useEffect } from 'react';
-import { Container, Box } from '@mui/material'
+import { Container, Box, Grid } from '@mui/material'
 import Header from './layout/AppHeader.js'
 import UserList from './components/UserList.js';
+import UserFilter from './components/UserFilter.js';
+import dot from 'dot-object'
 
 function App() {
 
   const [users, setUsers] = useState([]);
 
+  const [filters, setFilters] = useState({
+    filter: {
+      name: '',
+      email: '',
+      phone: '',
+    }
+  });
+
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(response => response.json())
       .then(data => setUsers(data))
-      .catch(error => console.error(error));
-  }, []);
+      .catch(error => console.error(error))
+  }, [])
 
+  const handleFilterChange = (key, val) => {
+    setFilters(prevFilters => {
+      const filters = { ...prevFilters }
+      dot.set(key, val, filters)
+      return filters
+    });
+  }
+
+  const filteredUsers = users.filter(user => {
+    const nameMatch = user.name.toLowerCase().includes(filters.filter.name.toLowerCase())
+    const emailMatch = user.email.toLowerCase().includes(filters.filter.email.toLowerCase())
+    const phoneMatch = user.phone.toLowerCase().includes(filters.filter.phone.toLowerCase())
+
+    return nameMatch && emailMatch && phoneMatch;
+  })
 
   return (
     <Fragment>
       <Header></Header>
       <Container maxWidth="xl">
         <Box marginTop={{ xs: 3 }} sx={{ height: 'calc(100vh - 64px)' }}>
-          <UserList users={users} />
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <UserFilter filter={filters.filter} onFilterChange={handleFilterChange} />
+            </Grid>
+          </Grid>
+          <UserList users={filteredUsers} />
         </Box>
       </Container>
     </Fragment>
---
+++ user-list/src/components/UserFilter.js
@@ -0,0 +1,49 @@
+import { Grid, FormControl, TextField } from '@mui/material';
+
+function UserFilter({ filter, onFilterChange }) {
+
+
+
+    return (
+        <Grid container spacing={2}>
+            <Grid item xs={12} md={4}>
+                <FormControl fullWidth>
+                    <TextField
+                        id="filter-name"
+                        name="name"
+                        label="Name"
+                        value={filter.name}
+                        fullWidth
+                        onChange={(e) => onFilterChange('filter.name', e.target.value)}
+                    />
+                </FormControl>
+            </Grid>
+            <Grid item xs={12} md={4}>
+                <FormControl fullWidth>
+                    <TextField
+                        id="filter-email"
+                        name="email"
+                        label="Email"
+                        value={filter.email}
+                        fullWidth
+                        onChange={(e) => onFilterChange('filter.email', e.target.value)}
+                    />
+                </FormControl>
+            </Grid>
+            <Grid item xs={12} md={4}>
+                <FormControl fullWidth>
+                    <TextField
+                        id="filter-phone"
+                        name="phone"
+                        label="Phone"
+                        value={filter.phone}
+                        fullWidth
+                        onChange={(e) => onFilterChange('filter.phone', e.target.value)}
+                    />
+                </FormControl>
+            </Grid>
+        </Grid>
+    );
+}
+
+export default UserFilter;
